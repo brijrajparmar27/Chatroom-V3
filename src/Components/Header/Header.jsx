c92ada604f6fb2a40d7e467f2e56d3b5a9c6dec5
Brijrajsinh parmar <brijrajparmar27@gmail.com>
2022-07-10 13:50:28 +0530

    added user dp change function

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,101 @@
+import useLogout from "../../Hooks/useLogout";
+import logo from "../../assets/images/logo.png";
+import avatar from "../../assets/images/avatar.svg";
+import { useRef, useState } from "react";
+import "./Header.css";
+import { useEffect } from "react";
+import useDP from "../../Hooks/useDP";
+import { storage } from "../../Firebase/config";
+
+const Header = ({ user }) => {
+  const { logout } = useLogout();
+  const [showPopup, setShowPopup] = useState(false);
+  const [image, setImage] = useState(null);
+  const { updateProfilePic,progress } = useDP();
+
+
+  
+
+  user && user.photoURL && console.log("photo url is :- ",user.photoURL);
+
+  const inputFile = useRef(null);
+
+  const onButtonClick = () => {
+    inputFile.current.click();
+  };
+
+  const handleImageChange = (e) => {
+    if (e.target.files[0]) {
+
+      setImage(e.target.files[0]);
+      updateProfilePic(e.target.files[0],user);
+   }
+  };
+
+  return (
+    <div className="header">
+      <div className="branding">
+        <img src={logo} alt="" />
+        <h1>Chatroom</h1>
+      </div>
+
+      <div className="user_profile_contain">
+        {
+          progress && <div className="progress_bar">
+            <div className="progress_completed" style={{flex:`${progress}`}}></div>
+          </div>
+        }
+        <div className="dp_container">
+        <img
+          src={user && user.photoURL ? user.photoURL : avatar}
+          className="user_avatar"
+          onClick={() => {
+            setShowPopup((prev) => !prev);
+          }}
+        />
+        </div>
+        {user && user.photoURL && console.log(user.photoURL)}
+        <h2
+          className="user_name"
+          onClick={() => {
+            setShowPopup((prev) => !prev);
+          }}
+        >
+          {user && user.displayName}
+        </h2>
+        {showPopup && (
+          <div
+            className="backdrop"
+            onClick={() => {
+              setShowPopup((prev) => !prev);
+            }}
+          ></div>
+        )}
+        {showPopup && (
+          <div className="popup">
+            <p className="popup_option" onClick={onButtonClick}>
+              Change Photo
+            </p>
+            <input
+              type="file"
+              id="file"
+              ref={inputFile}
+              style={{ display: "none" }}
+              onChange={handleImageChange}
+            />
+            <a
+              href="mailto:brijrajparmaromegab32@gmail.com"
+              className="popup_option"
+            >
+              Report bug
+            </a>
+            <button className="logout_btn" onClick={logout}>
+              Logout
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+export default Header;
---
+++ src/Hooks/useDP.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { auth, storage } from "../Firebase/config";
+
+const useDP = () => {
+  
+  const [progress, setProgress] = useState();
+
+  useEffect(()=>{
+    if(progress==1)
+    {
+      setProgress(null);
+    }
+  },[progress])
+
+  const updateProfilePic = (imageFile,user) => {
+    console.log("update function");
+    const uploadTask = storage.ref(`ProfilePictures/${user.uid}.png`).put(imageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const prog = 
+          (snapshot.bytesTransferred / snapshot.totalBytes);
+        setProgress(prog);
+      },
+      (error) => console.log(error),
+      () => {
+        storage
+          .ref("ProfilePictures")
+          .child(`${user.uid}.png`)
+          .getDownloadURL()
+          .then((url) => {
+            console.log(url);
+            user.updateProfile({ photoURL:url });
+          });
+      }
+    );
+  };
+  return { updateProfilePic, progress };
+};
+export default useDP;
