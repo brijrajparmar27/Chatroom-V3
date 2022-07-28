0ff6d5e0a4850cc26435a3eee8b22b3b57115313
Brijrajsinh parmar <brijrajparmar27@gmail.com>
2022-07-28 20:08:30 +0530

    added no messages found animation

--- src/Components/RightPanel/RightPanel.jsx
+++ src/Components/RightPanel/RightPanel.jsx
@@ -2,24 +2,54 @@ import "./RightPanel.css";
 import { AiOutlinePaperClip, AiOutlineSend } from "react-icons/ai";
 import useRoomContext from "../../Hooks/useRoomContext";
 import useCollection from "../../Hooks/useCollection";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import useAuthContext from "../../Hooks/useAuthContext";
 import Bubble from "../Bubble/Bubble";
+import useImage from "../../Hooks/useImage";
 
 const RightPanel = () => {
   const { currentRoom } = useRoomContext();
   const { chats, Send, loading } = useCollection(currentRoom.roomid);
+  const { updateProfilePic, progress } = useImage();
   const { user } = useAuthContext();
-  const [textmsg, setTextmsg] = useState(false);
   const [textImg, setTextImg] = useState(false);
+  const [Url, setUrl] = useState();
+  const inputFile = useRef(null);
+  const [enableSend, setEnableSend] = useState(true);
 
-  const date = new Date();
+  const onButtonClick = () => {
+    console.log("file selector opened");
+    inputFile.current.click();
+  };
+
+  const onUploadComplete = (url) => {
+    console.log("url set");
+    setUrl(url);
+    console.log("button enabled");
+    setEnableSend(true);
+    // setTextImg(null);
+  };
+
+  const handleImageChange = async (e) => {
+    if (e.target.files[0]) {
+      console.log("image selected");
+      console.log("button disabled");
+      setEnableSend(false);
+      let imgname = `${currentRoom.roomid}_${user.uid}_${Math.round(
+        Math.random() * 100000
+      )}`;
+      setTextImg(e.target.files[0]);
+      updateProfilePic(false, e.target.files[0], user, "ChatImages", imgname, {
+        onComplete: onUploadComplete,
+      });
+    }
+  };
 
   const createJson = ({ image, text }) => {
     const dataJson = {
       timestamp: new Date(),
       uid: user.uid,
-      image: image ? "url" : "",
+      image: image ? Url : "",
       text: text ? text : "",
       roomid: currentRoom.roomid,
       displayName: user.displayName,
@@ -31,16 +61,20 @@ const RightPanel = () => {
 
   const handleSend = (e) => {
     e.preventDefault();
-    const msg = e.target.msg.value;
-    if (!textImg && !msg) {
-      return;
-    }
+    if (enableSend) {
+      console.log("send button Clicked");
+      const msg = e.target.msg.value;
+      if (!textImg && !msg) {
+        return;
+      }
 
-    let dataJson = createJson({ image: textImg, text: msg });
-    Send(dataJson);
-    e.target.reset();
-    setTextmsg(false);
-    setTextImg(false);
+      let dataJson = createJson({ image: textImg, text: msg });
+      console.log(dataJson);
+      Send(dataJson);
+      e.target.reset();
+      // setTextmsg(false);
+      setTextImg(false);
+    }
   };
 
   return (
@@ -55,8 +89,21 @@ const RightPanel = () => {
             <div className="chats_contain scrollbar" id="style-1">
               {chats &&
                 chats.map((each) => {
-                  return <Bubble each={each} />;
+                  return <Bubble each={each} key={each.id} />;
                 })}
+
+              {!loading && chats && chats.length == 0 && (
+                <div className="loader">
+                  <lottie-player
+                    src="https://assets1.lottiefiles.com/packages/lf20_ydo1amjm.json"
+                    background="transparent"
+                    speed="1"
+                    style={{ width: "50%"}}
+                    loop
+                    autoplay
+                  ></lottie-player>
+                </div>
+              )}
             </div>
 
             <div className="input_bar_contain">
@@ -72,13 +119,40 @@ const RightPanel = () => {
                     className="msg_textbox"
                     name="msg"
                   />
-                  <div className="file_selector_btn">
+                  <div className="file_selector_btn" onClick={onButtonClick}>
                     <AiOutlinePaperClip
-                      style={{ fontSize: "26px" }}
+                      style={{
+                        fontSize: "26px",
+                        color: textImg ? "blue" : "grey",
+                      }}
                     ></AiOutlinePaperClip>
                   </div>
-                  <button type="submit" className="msg_submit_btn">
-                    <AiOutlineSend style={{ fontSize: "22px" }}></AiOutlineSend>
+                  <input
+                    type="file"
+                    id="file"
+                    ref={inputFile}
+                    style={{ display: "none" }}
+                    onChange={handleImageChange}
+                  />
+                  <button
+                    type="submit"
+                    className="msg_submit_btn"
+                    disabled={enableSend ? false : true}
+                  >
+                    {enableSend ? (
+                      <AiOutlineSend
+                        style={{ fontSize: "22px" }}
+                      ></AiOutlineSend>
+                    ) : (
+                      <lottie-player
+                        src="https://assets4.lottiefiles.com/private_files/lf30_06kvvo5n.json"
+                        background="transparent"
+                        speed="1"
+                        style={{ width: "30px", height: "30px" }}
+                        loop
+                        autoplay
+                      ></lottie-player>
+                    )}
                   </button>
                 </form>
               </div>
